a2a89655907502ea324fbc533b363de115d47bfe
Nir Valtman <nir@arnica.io>
2023-05-10 22:49:25 -0400

    [secret masked by Arnica] added a secret

---
+++ list.py
@@ -0,0 +1,16 @@
+import boto3
+
+aws_access_key_id = '********************'
+aws_secret_access_key = '****************************************'
+output = json
+region = us-east-2
+
+# Create an S3 client with your credentials
+s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+# Call the list_buckets method to get a list of all S3 buckets
+response = s3.list_buckets()
+
+# Print the name of each bucket
+for bucket in response['Buckets']:
+    print(bucket['Name'])
